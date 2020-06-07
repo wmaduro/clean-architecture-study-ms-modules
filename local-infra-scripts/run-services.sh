3ca69330b9e8d4632c8e0d1be5a1a387ddfd8083
welerson maduro <wmaduro@gmail.com>
2020-06-08 11:45:58 +1200

    scripts

---
+++ local-infra-scripts/run-services.sh
@@ -0,0 +1,2 @@
+java -jar ~/hd2/Estudo/CONCEPTS-PRJ/architecture-study/clean-architecture-study-ms-ws/clean-architecture-study-ms-modules/cas-ms-file-parser/target/cas-ms-file-parser-0.0.1-SNAPSHOT.jar &
+java -jar ~/hd2/Estudo/CONCEPTS-PRJ/architecture-study/clean-architecture-study-ms-ws/clean-architecture-study-ms-modules/cas-ms-storage/target/cas-ms-storage-0.0.1-SNAPSHOT.jar &
