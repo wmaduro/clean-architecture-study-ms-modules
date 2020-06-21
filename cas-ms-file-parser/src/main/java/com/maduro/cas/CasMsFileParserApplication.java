5fed1dbead8274670c53fbca7e49fa5bdddc8f0e
welerson maduro <wmaduro@gmail.com>
2020-06-22 10:37:52 +1200

    cas-ms-file-parser - ignore

--- cas-ms-file-parser/src/main/java/com/maduro/cas/CasMsFileParserApplication.java
+++ cas-ms-file-parser/src/main/java/com/maduro/cas/CasMsFileParserApplication.java
@@ -1,13 +1,24 @@
 package com.maduro.cas;
 
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class CasMsFileParserApplication {
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(CasMsFileParserApplication.class, args);
 	}
 
+//	@Bean
+//	ApplicationRunner applicationRunner(Environment environment) {
+//		return args -> {
+//			System.out.println("3--> " + environment.getProperty("cas.ms.storage.port", "liooooooooo"));
+//		};
+//	}
+	
 }
