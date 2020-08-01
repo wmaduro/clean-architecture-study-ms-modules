6a9004adac86bb6c89133325d42fe69d537d5377
welerson maduro <wmaduro@gmail.com>
2020-08-02 11:58:34 +1200

    cas-ms-storage - LOGSTASH

--- cas-ms-storage/src/main/java/com/maduro/cas/CasMsStorageApplication.java
+++ cas-ms-storage/src/main/java/com/maduro/cas/CasMsStorageApplication.java
@@ -1,13 +1,27 @@
 package com.maduro.cas;
 
+import java.util.Date;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 
 @SpringBootApplication
+@EnableAsync
+@EnableScheduling
 public class CasMsStorageApplication {
+	Logger logger = LoggerFactory.getLogger(CasMsStorageApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(CasMsStorageApplication.class, args);
 	}
 
+	@Scheduled(fixedDelay=5000)
+	public void doSomething() {
+		logger.info("teste... "+new Date());
+		}
 }
